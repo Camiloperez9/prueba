a88ff24e18a6e786d9f2113f202949bce39540de
Camiloperez9 <138942751+Camiloperez9@users.noreply.github.com>
2024-07-20 21:30:26 -0300

    cree el controlador

---
+++ app/Http/Controllers/personitaController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Personita;
+
+class personitaController extends Controller
+{
+    public function Listar(Request $request){
+        return personita::all();
+    }
+
+    public function ListarUno(Request $request, $idPersona){
+        return personita::findOrFail($idPersona);
+
+    }
+
+    public function Eliminar(Request $request, $idPersona){
+        $persona = personita::findOrFail($idPersona);
+        $persona -> delete();
+
+        return [ "mensaje" => "Persona $idPersona a sido eliminada."];
+
+
+
+    }
+    public function Insertar(Request $request){
+        $persona = new personita;
+        $persona -> nombre = $request -> post("nombre");
+        $persona -> apellido = $request -> post("apellido");
+        $persona -> telefono = $request -> post("telefono");
+        
+        $persona -> save();
+
+        return $persona;
+    }
+
+    public function Modificar(Request $request, $idPersona){
+        $persona = personita::findOrFail($idPersona);
+        $persona -> nombre = $request -> post("nombre");
+        $persona -> apellido = $request -> post("apellido");
+        $persona -> telefono = $request -> post("telefono");
+
+        $persona -> save();
+        return $persona;
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\personitaController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/personita', [personitaController::class, "ListarTodas"]);
+Route::get('/personita/{d}', [personitaController::class, "ListarUna"]);
+Route::post('/personita', [personitaController::class, "Crear"]);
+Route::delete('/personita/{d}', [personitaController::class, "Eliminar"]);
+Route::put('/personita/{d}', [personitaController::class, "Modificar"]);
